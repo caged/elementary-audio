7f1db26a84a411110b4cc7d9f122176fb1c0ba24
Justin Palmer <encytemedia@gmail.com>
2021-11-03 12:56:08 -0700

    break it out into a logical set of components

---
+++ src/Plugin.svelte
@@ -0,0 +1,17 @@
+<script>
+  import PowerButton from "./PowerButton.svelte";
+</script>
+
+<div class="plugin">
+  <PowerButton />
+</div>
+
+<style>
+  .plugin {
+    width: 500px;
+    height: 200px;
+    background-color: #f7f7f7;
+    box-sizing: border-box;
+    padding: 10px;
+  }
+</style>
