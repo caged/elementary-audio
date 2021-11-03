36e0be81c222eaa12f4913750c58477a71ab33ba
Justin Palmer <encytemedia@gmail.com>
2021-11-02 21:52:46 -0700

    add some ui

--- src/Application.svelte
+++ src/Application.svelte
@@ -3,10 +3,15 @@
 
   import {
     ElementaryWebAudioRenderer as core,
-    el,
+    sugar,
   } from "@nick-thompson/elementary";
+  import supersaw from "./supersaw";
 
   let loaded = false;
+
+  let voices = 3;
+  let spread = 5;
+  let frequency = 300;
 
   onMount(async () => {
     const actx = new (window.AudioContext || window.webkitAudioContext)();
@@ -21,15 +26,69 @@
 
   core.on("load", () => (loaded = true));
 
-  function play() {
-    core.render(el.mul(0.3, el.cycle(440)), el.mul(0.3, el.cycle(441)));
+  function render() {
+    const out = sugar(supersaw, { voices, spread, frequency });
+    core.render(out, out);
   }
 </script>
 
-<div>
-  <button on:click={play}>play</button>
+<div class="form">
+  <label for="frequency">Frequency</label>
+  <input
+    bind:value={frequency}
+    on:change={render}
+    type="range"
+    min="5"
+    max="500"
+    step="1"
+    class="range"
+  />
+  <span class="val">{frequency}</span>
+  <label for="voices">Voices</label>
+  <input
+    bind:value={voices}
+    on:change={render}
+    type="range"
+    min="1"
+    max="10"
+    step="1"
+    class="range"
+  />
+  <span class="val">{voices}</span>
+
+  <label for="spread">Spread</label>
+  <input
+    bind:value={spread}
+    on:change={render}
+    type="range"
+    min="1"
+    max="10"
+    step="1"
+    class="range"
+  />
+  <span class="val">{spread}</span>
 </div>
+<button on:click={render}>Play</button>
 
 <style>
-  /* css will go here */
+  .range {
+    width: 300px;
+  }
+
+  div.form {
+    padding: 20px;
+    display: grid;
+    grid-template-columns: max-content max-content max-content;
+    grid-gap: 20px;
+  }
+  div.form label {
+    text-align: right;
+    font-weight: bold;
+  }
+
+  .val {
+    font-family: ui-monospace, Menlo, Monaco, "Cascadia Mono", "Segoe UI Mono",
+      "Roboto Mono", "Courier New", monospace;
+    color: #333;
+  }
 </style>
---
+++ src/supersaw.js
@@ -0,0 +1,13 @@
+import { ElementaryWebAudioRenderer, el } from "@nick-thompson/elementary";
+
+// From https://docs.elementary.audio/guides/writing_reusable_components#memoization
+function supersaw({ props, context, children }) {
+  let saws = [];
+  for (let i = 0; i < props.voices; ++i) {
+    let detune = (i / props.voices) * props.spread;
+    saws.push(el.sub(el.phasor(props.frequency + detune), 0.5));
+  }
+  return el.add(saws);
+}
+// It's easy!
+export default ElementaryWebAudioRenderer.memo(supersaw);
