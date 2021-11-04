df8e8e6faafc7e9c90d7e13ea0455af17fc1690c
caged <justin@labratrevenge.com>
2021-11-04 02:19:11 +0000

    Deploying to gh-pages from  @ 2f56cb813967b11aed9e2385123a81cc8621a579 🚀

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,7 +7,8 @@ export default {
   optimize: {
     bundle: true,
     minify: true,
-    target: "es2018"
+    target: "es2018",
+    treeshake: false
   },
   packageOptions: {
     polyfillNode: true
