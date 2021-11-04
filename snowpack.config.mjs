65dd5f50c43d90d6c68cf1b3730b02a0db4391a3
Justin Palmer <encytemedia@gmail.com>
2021-11-03 20:42:33 -0700

    try webpack build again

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {},
-  plugins: ["@snowpack/plugin-svelte"],
+  plugins: ["@snowpack/plugin-svelte", "@snowpack/plugin-webpack"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
-    bundle: true,
-    minify: false,
-    target: "es2018",
+    /* */
   },
   packageOptions: {
     polyfillNode: true,
